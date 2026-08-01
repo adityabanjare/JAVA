b2781fb017baaae16ac0c44e59cd8cf0379d0c64
AdityaBanjare <adityabanjare4444@gmail.com>
2026-08-01 10:15:40 +0530

    second commit

---
+++ .idea/EvenOdd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class  EvenOdd
+{
+    public static void checkEvenOdd(num){
+        if(num%2==0){
+            System.out.println("num is : Even");
+        }
+        else{
+            System.out.println("num is Odd");
+        }
+    }
+    public static void main(String[] args){
+     Scanner sc = new Scanner(System.in);
+     System.out.println("enter the num");
+        int num = sc.nextInt();
+        checkEvenOdd();
+        sc.close();
+
+    }
+
+
+}
