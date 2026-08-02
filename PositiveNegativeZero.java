6be6aa1b97fb9e2cb1256161b20939d2c7dd4594
AdityaBanjare <adityabanjare4444@gmail.com>
2026-08-02 13:40:48 +0530

    second commit

---
+++ PositiveNegativeZero.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PositiveNegativeZero {
+    public static void positiveNegativeZero(int num){
+        if(num>0){
+            System.out.println("positive number");
+        }
+        else if(num<0){
+            System.out.println("negative number");
+        }
+        else{
+            System.out.println("zero number");
+        }
+    }
+     public static void main(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the num");
+        int num=sc.nextInt();
+        positiveNegativeZero(num);
+        sc.close();
+     }
+
+}
