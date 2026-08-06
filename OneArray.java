c713ec3f5336e9167d4796f7d01aade087781e6d
AdityaBanjare <adityabanjare4444@gmail.com>
2026-08-06 13:31:07 +0530

    second commit

---
+++ OneArray.java
@@ -0,0 +1,13 @@
+public class OneArray {
+    public static void main(String[] args) {
+        int a[]=new int[3];
+        a[0]=10;
+        a[1]=20;
+        a[2]=30;
+        System.out.println("display arry");
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+    
+}
