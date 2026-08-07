d3da9404431703b1d43757d614c4c97c8439586a
AdityaBanjare <adityabanjare4444@gmail.com>
2026-08-07 14:20:05 +0530

    second commit

---
+++ Arry3.java
@@ -0,0 +1,14 @@
+public class arry3 {
+    public static void main(String[] args) {
+        int a[]=new int[5];
+        for(int i=0;i<a.length; i++){
+            a[i]=i*2;
+            System.out.println("display Arry");
+            // for(i=0; i<alength;i++){
+            //System.out.println("a[i]");
+            }
+            for (int i:a)
+            System.out.println(i);
+        }
+    
+}
