f9e36355b669ee5cf0b396d7100e3d10ef61aa33
AdityaBanjare <adityabanjare4444@gmail.com>
2026-08-07 14:27:05 +0530

    second commit

---
+++ HelloArray.java
@@ -0,0 +1,8 @@
+public class HelloArray {
+    public static void main(String[] args) {
+        String str[]={"hello","how","are","you"};
+        for(String s:str)
+            System.out.println(s);
+    }
+    
+}
